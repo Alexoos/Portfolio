76436748b1bd7247a97418f6b9c09d0777841cf8
Alexis Bertin <alex.bertin03@gmail.com>
2023-03-30 17:38:35 +0200

    Ajout projet Parc Informatique + modifications UX

--- src/components/projects/SliderParc.js
+++ src/components/projects/SliderParc.js
@@ -1,33 +1,43 @@
-import React from 'react'
+import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
-import Parc1 from '../../images/parc_1.png';
-import Parc2 from '../../images/parc_2.PNG';
+import { Carousel } from "react-responsive-carousel";
+import Parc1 from "../../images/parc_1.png";
+import Parc2 from "../../images/parc_2.PNG";
+import Parc3 from "../../images/parc_3.PNG";
+import Parc4 from "../../images/parc_4.PNG";
 export default function Slider() {
   const datas = [
     {
-        id:1,
-        image: Parc1,
-      },
-      {
-        id:2,
-        image: Parc2,
-      },
-    ]
-    return (
-      <Carousel
-        autoPlay 
-        interval={5000} 
-        infiniteLoop 
-        thumbWidth={80} 
-        showIndicators={true} 
-        showStatus={false}
-      >
-        {datas.map(slide =>(
-          <div key={slide.id}>
-            <img  src ={slide.image} alt=""></img>
-          </div>
-        ))}
-      </Carousel>
-    )
-  }
+      id: 1,
+      image: Parc1,
+    },
+    {
+      id: 2,
+      image: Parc2,
+    },
+    {
+      id: 3,
+      image: Parc3,
+    },   
+    {
+      id: 4,
+      image: Parc4,
+    },
+  ];
+  return (
+    <Carousel
+      autoPlay
+      interval={5000}
+      infiniteLoop
+      thumbWidth={80}
+      showIndicators={true}
+      showStatus={false}
+    >
+      {datas.map((slide) => (
+        <div key={slide.id}>
+          <img src={slide.image} alt=""></img>
+        </div>
+      ))}
+    </Carousel>
+  );
+}
--- src/pages/Parc.js
+++ src/pages/Parc.js
@@ -13,8 +13,7 @@ const Parc = () => {
                     <h1 className='titre_parc'>Parc Informatique</h1>
                     <SliderParc/>
                     <p className='paragrapheParc'>
-                       Refonte du parc informatique (Avant: image 1 et 2, Apres: image 3 et 4), ce parc informatique permet de stocker toutes informations nécessaires pour la maintenance informatique d'une entreprise.
-                       La refonte a étais faite en PHP/Bootstrap. 
+                       Refonte du parc informatique (Avant: image 1 et 2, Apres: image 3 et 4), ce parc informatique permet de stocker toutes informations nécessaires pour la maintenance informatique d'entreprise, la refonte a permis au parc informatique d'être accessible de n'importe où (responsive).La refonte a été faite en PHP/Bootstrap. 
                     </p>
                 </div>
             </div>
